e961a1326b0a0a3d3a3a5ddcae16f336578f3933
alex444 <alexbatenkin@bk.ru>
2024-05-28 23:53:12 +0400

    slider done

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
-const slider = document.querySelector(".slider");
-const buttonLeft = document.querySelector(".buttonLeft");
-const buttonRight = document.querySelector(".buttonRight");
+const divForShadow = document.querySelector(".divForShadow");
 
-buttonRight.addEventListener("click", () => {
-  console.log("click");
-  slider.style.overflow = "visible";
-  slider.style.transform = "translate(-100vw)";
+// let x = divForShadow.getBoundingClientRect().left;
+// let y = divForShadow.getBoundingClientRect().top;
+// console.log(x, y);
+// placeDiv(x, y);
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    location.href = "#slide1";
+  } else if (event.key === "ArrowRight") {
+    location.href = "#slide2";
+  }
 });
-buttonLeft.addEventListener("click", () => {
-  slider.style.overflow = "hidden";
-  slider.style.transform = "translate(0vw)";
-});
+// console.log(divForShadow.getBoundingClientRect().top);
+// function placeDiv(x_pos, y_pos) {
+//   let d = document.querySelector(".buttons");
+//   //   d.style.position = "absolute";
+//   d.style.left = x_pos + 36 + "px";
+//   d.style.top = y_pos - 10 + "px";
+// }
+// window.addEventListener("resize", () => {
+//   let x = divForShadow.getBoundingClientRect().left;
+//   let y = divForShadow.getBoundingClientRect().top;
+//   console.log(x, y);
+//   placeDiv(x, y);
+// });
